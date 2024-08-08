495ff380288d70de00ddc58304f44ab6e98f1a1e
shizukani9 <tatianamayorga9999@gmail.com>
2024-08-07 20:27:33 -0400

    login_credecials erika sucessfull

--- main/ui/login_page.js
+++ main/ui/login_page.js
@@ -5,6 +5,8 @@ class LoginPage{
     passwordInput = By.css('form #credentials_password');
     nextButton = By.css('form .app_signin_action_button');
     loginButton = By.css('form label ~ .app_signin_action_button');
-}
 
+    //cookies button
+    cookiesButton = By.css('.app_cookie_policy_accept_button');
+}
 module.exports = new LoginPage();
