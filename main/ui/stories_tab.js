63ca27c166a4dc32c4a4e58188783142fc130e62
Valeria220499 <20170027@est.umss.edu>
2024-08-11 01:18:29 -0400

    actualizacion del archivo stories_tab.js

--- main/ui/stories_tab.js
+++ main/ui/stories_tab.js
@@ -3,6 +3,9 @@ const { By } = require("selenium-webdriver");
 class StoriesTab{
     addStoryButton = By.css('.backlog.current_backlog button[title="Add Story"]');
     previewStoryItemRow = By.css('.backlog div[data-aid="StoryPreviewItem"] span[class="tracker_markup"]');
+    deleteStoryButton = By.css('button.autosaves.delete[title="Delete this story"]');
+    confirmDeleteButton = By.css('button.SMkCk__Button.SSqkh__Button--warning[data-aid="DeleteButton"]');
+    emptyMessageText = By.css('span.empty_message_text');
 }
 
 module.exports = new StoriesTab();
