7d12b309e958152797ec29215a9a47e5bd7b35d7
shizukani9 <tatianamayorga9999@gmail.com>
2024-08-10 23:10:42 -0400

    cambios en stories tab

--- main/ui/stories_tab.js
+++ main/ui/stories_tab.js
@@ -1,7 +1,7 @@
 const { By } = require("selenium-webdriver");
 
 class StoriesTab{
-    addStoryButton = By.css('.backlog.current_backlog button[title="Add Story"]' );
+    addStoryButton = By.css('.backlog.current_backlog button[title="Add Story"]');
     previewStoryItemRow = By.css('.backlog div[data-aid="StoryPreviewItem"] span[class="tracker_markup"]');
     logoImage = By.css('img.headerLogo__image');
     profileDropdownButton = By.xpath('/html/body/div[4]/header/div[1]/div/div/header/ul/li[3]/div/div/button');
