3dea132091e49feea7f9e4dbd9cd865ee7d83ba9
Valeria220499 <20170027@est.umss.edu>
2024-08-11 01:20:15 -0400

    automatizacion de feature del eliminar historia de tipo bu

---
+++ features/support/delete_bug_story_steps.js
@@ -0,0 +1,30 @@
+const { Then } = require("@cucumber/cucumber");
+const DriverFactory = require("../../core/ui/driverFactory");
+const StoriesTab = require("../../main/ui/stories_tab");
+const chai = require('chai');
+const expect = chai.expect;
+const { until } = require("selenium-webdriver");
+const configuration = require("../../configuration.json")
+
+Then('I have a bug story in the backlog', async function() {
+    // Verificar la existencia del botón 'Add Story'
+    const addStoryButton = await DriverFactory.myDriver.wait(until.elementLocated(StoriesTab.addStoryButton), configuration.browser.timeout);
+    expect(await addStoryButton.isDisplayed()).to.be.true;
+});
+
+Then('I delete the bug type story from the backlog', async function() {
+    // Esperar a que el botón 'Delete' esté habilitado y luego hacer clic en él
+    const deleteStoryButton = await DriverFactory.myDriver.wait(until.elementLocated(StoriesTab.deleteStoryButton), configuration.browser.timeout);
+    await deleteStoryButton.click();
+
+    // Esperar a que el botón de confirmación de eliminación esté visible y luego hacer clic en él
+    const confirmDeleteButton = await DriverFactory.myDriver.wait(until.elementLocated(StoriesTab.confirmDeleteButton), configuration.browser.timeout);
+    await DriverFactory.myDriver.wait(until.elementIsVisible(confirmDeleteButton), configuration.browser.timeout);
+    await confirmDeleteButton.click();
+});
+
+Then('I should see the backlog empty', async function() {
+    // Verificar que el mensaje de backlog vacío esté presente
+    const emptyMessageText = await DriverFactory.myDriver.wait(until.elementLocated(StoriesTab.emptyMessageText), configuration.browser.timeout);
+    expect(await emptyMessageText.isDisplayed()).to.be.true;
+});
