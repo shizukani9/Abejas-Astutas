e0819d129cf8f1a7344afea2494b29b2bebc9fd3
shizukani9 <tatianamayorga9999@gmail.com>
2024-08-11 15:22:44 -0400

    Feature eliminacion de un proyecto nuevo

---
+++ features/support/delete_project_steps.js
@@ -0,0 +1,28 @@
+const { When, Then } = require("@cucumber/cucumber");
+const DriverFactory = require("../../core/ui/driverFactory");
+const StoriesTab = require("../../main/ui/stories_tab");
+const IntroductionPage = require("../../main/ui/introduction_page");
+const RandomValues = require("../../features/support/random_values");
+const { until, Key } = require("selenium-webdriver");
+const configuration = require("../../configuration.json");
+const ProjectSettingsPage = require("../../main/ui/project_settings_page");
+let chai = require('chai');
+let expect = chai.expect;
+
+When('I delete a new project', async function(){
+    console.log("Starting to delete the new first project");
+    await DriverFactory.myDriver.get("https://www.pivotaltracker.com/projects/"+this.firstProjectId+"/settings");
+    const deleteLink = await DriverFactory.myDriver.wait(until.elementLocated(ProjectSettingsPage.deleteLink), configuration.browser.timeout);
+    deleteLink.sendKeys(Key.SHIFT);
+    await DriverFactory.myDriver.wait(until.elementIsVisible(deleteLink), configuration.browser.timeout);
+    await deleteLink.click();
+    const deleteButton = await DriverFactory.myDriver.wait(until.elementLocated(ProjectSettingsPage.deleteButton));
+    await deleteButton.click();
+});
+
+Then('I should see the Introduction page empty', async function(){
+    console.log("Verifying the current page is the Introduction Page");
+    await DriverFactory.myDriver.wait(until.urlIs("https://www.pivotaltracker.com/introduction"), configuration.browser.timeout);
+    const currentUrl = await DriverFactory.myDriver.getCurrentUrl();
+    expect(currentUrl).to.equal("https://www.pivotaltracker.com/introduction");
+});
