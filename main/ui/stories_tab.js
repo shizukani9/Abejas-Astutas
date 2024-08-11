98d55799a0e92319ac6d69aea23ba8b6f07a6e2d
shizukani9 <tatianamayorga9999@gmail.com>
2024-08-10 22:57:26 -0400

    aumento de elementos

--- main/ui/stories_tab.js
+++ main/ui/stories_tab.js
@@ -1,8 +1,11 @@
 const { By } = require("selenium-webdriver");
 
 class StoriesTab{
-    addStoryButton = By.css('.backlog.current_backlog button[title="Add Story"]');
+    addStoryButton = By.css('.backlog.current_backlog button[title="Add Story"]' );
     previewStoryItemRow = By.css('.backlog div[data-aid="StoryPreviewItem"] span[class="tracker_markup"]');
+    logoImage = By.css('img.headerLogo__image');
+    profileDropdownButton = By.xpath('/html/body/div[4]/header/div[1]/div/div/header/ul/li[3]/div/div/button');
+    signOutButton = By.xpath('/html/body/div[4]/header/div[1]/div/div/header/ul/li[3]/div/div/div[2]/div/div/form/button');
 }
 
 module.exports = new StoriesTab();
