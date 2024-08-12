968ba6b65ed50e38d631ea7feeef63e3fe261374
Jenny Reque <73964585+Jhen123@users.noreply.github.com>
2024-08-11 23:48:17 -0400

    Feature agregar automatizacion eliminar historia tipo feature (#20)
    * Eliminar una historia de tipo Feature
    
    * cambio
    
    * solucionado
    
    * cambio hooks
    
    * cambio hooks
    
    * cambios

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -99,29 +99,30 @@ After({ tags: "@deleteFirstProject" }, async function(scenario){
     }
 });
 
-Before({ tags: "@createBugStory" }, async function(){
-    console.log("Starting to create a bug story");
-    if (!this.createBugStoryHook){
+Before({ tags: "@createBugStory or @createFeatureStory" }, async function(scenario){
+    console.log("Starting to create a feature story");
+    if (!this.createFeatureStoryHook){
         const addStoryButton = await DriverFactory.myDriver.wait(until.elementLocated(StoriesTab.addStoryButton));
         await DriverFactory.myDriver.wait(until.elementIsVisible(addStoryButton), configuration.browser.timeout);
         await addStoryButton.click();
         const storyTitleTextField = await DriverFactory.myDriver.wait(until.elementLocated(StoryPanel.storyTitleTextField));
         const storyTypeDropdown = await DriverFactory.myDriver.wait(until.elementLocated(StoryPanel.storyTypeDropdown));
-        this.firstStoryBugName = RandomValues.alphanumeric(6);       
-        await storyTitleTextField.sendKeys(this.firstStoryBugName);
+        this.firstStoryFeatureName = RandomValues.alphanumeric(6);       
+        await storyTitleTextField.sendKeys(this.firstStoryFeatureName);
         await storyTypeDropdown.click();
-        StoryPanel.locatorAux.value = StoryPanel.storyOptionInDropdown.value.replace("{0}", "bug");
+        const tags = scenario.pickle.tags; 
+        StoryPanel.locatorAux.value = StoryPanel.storyOptionInDropdown.value.replace("{0}", ((!!tags.find(tag => { return tag.name === '@createBugStory'; }) ? "bug":"feature")));
         const optionSelectedInDropdown = await DriverFactory.myDriver.wait(until.elementLocated(StoryPanel.locatorAux));
         await optionSelectedInDropdown.click();
         const saveButton = await DriverFactory.myDriver.wait(until.elementLocated(StoryPanel.saveButton));
         await saveButton.click();
         await DriverFactory.myDriver.wait(until.elementIsVisible(addStoryButton), configuration.browser.timeout);
         let storyItem = await DriverFactory.myDriver.wait(until.elementLocated(StoriesTab.previewStoryItemRow));
-        storyItem = await DriverFactory.myDriver.wait(until.elementTextContains(storyItem, this.firstStoryBugName));
+        storyItem = await DriverFactory.myDriver.wait(until.elementTextContains(storyItem, this.firstStoryFeatureName));
         await storyItem.click();
-        console.log("Bug story created successfully in backlog with title:", this.firstStoryBugName);
+        console.log("Feature story created successfully in backlog with title:", this.firstStoryFeatureName);
     }
-    this.createBugStoryHook = true;
+    this.createFeatureStoryHook = true;
 });
 
 AfterAll({ tags: "@ui" },async function(){
